d8eb3797ea2fed9dc521ba24fa3759e94569a14c
tess-antonsson <78737757+tess-antonsson@users.noreply.github.com>
2021-03-04 10:28:18 +0100

    Changed tags from dictionary to list

--- hk_ros_2021/scripts/subscriber_node.py
+++ hk_ros_2021/scripts/subscriber_node.py
@@ -20,8 +20,7 @@ listen = tf.TransformListener()
 filename = "latest_output_file.yaml"
 filepath = rospkg.RosPack().get_path('hk_ros_2021') + '/exported_detection_logs/'
 #detections = []
-taglist={}
-
+taglist= [None] * 10
 
 def Aprildetections(msg):
 
@@ -31,28 +30,26 @@ def Aprildetections(msg):
         #with open(filepath + filename, 'w') as outfile:
     while not rospy.is_shutdown():
 
-
         for tagID in alltags:
             coordinates = tagTransform(tagID)
+            IDnum = int(remove_prefix(tagID, "tag_"))
+            #print(IDnum)
 
             if coordinates is not None:
-                        #print('ID: ' +  tagID + ', coordinates: ' + str(coordinates))
-                        #detections.append({"obj_type": "A", "XY_pos: " : str(coordinates), " Tag: " : tagID})
 
-                #coordinates = round(coordinates, 3)
                 strang = "XY_pos: " + str(coordinates) + ", obj_type: A"
-                taglist[tagID] = strang
-                    #Append(taglist)
-                    #pub.publish(json.dumps({'coords' : taglist[tagName],'name': tagName}))
+
+                taglist[IDnum] = strang
 
                 print(taglist)
-
-                        # strang = "obj_type: A, XY_pos:" + str(coordinates) + "Tag:" + str(tagID)
-                        # yaml.safe_dump(strang, outfile)
 
         rate.sleep()
         #rospy.spin()
 
+def remove_prefix(text, prefix):
+    if text.startswith(prefix):
+        return text[len(prefix):]
+    return text
 
 def tagTransform(tagId):
     try:
